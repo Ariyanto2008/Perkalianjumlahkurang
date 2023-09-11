135dd8029d35a4c9aee7d286cbd31c7a92629233
Ariyanto2008 <144643032+Ariyanto2008@users.noreply.github.com>
2023-09-11 10:19:14 +0700

    Create Perkalianjumlahkurang.cpp

---
+++ Perkalianjumlahkurang.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main( ){
+      int a,b;
+       cout << "PEMROGRAM OPERASI ARIMATIKA \n";
+      cout<<"masukan angka pertama: ";
+      cin>>a;
+      cout<<"masukan angka kedua ";
+      cin>>b;
+      cout<<"hasil penjumlahan: "<< a + b << endl;
+      cout<<"hasil pengurangan: "<< a - b << endl;
+   cout<<"hasil perkalian: "<< a * b << endl;
+   cout<<"hasil pembagian: "<< a / b << endl;
+      cout<<"hasil modulus: "<< a % b << endl;
+      cout<<"luas : "<< a / b << endl;
+       return 0;
+}
